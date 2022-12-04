dc529a1d05b6e27d9e0a36b931e246f5cd6792ef
Geofrey-25 <geofreyonyango167@gmail.com>
2022-12-04 19:47:30 +0300

    h

--- js/script.js
+++ js/script.js
@@ -26,3 +26,42 @@ const sr = ScrollReveal ({
 sr.reveal('.home-text',{delay:190, origin:'bottom'})
 
 sr.reveal('.about,.services,.portfolio,.contact',{delay:200, origin:'bottom'})
+
+
+// JavaScript code for generating random notifications
+function generateRandomNotification() {
+	// Create an array of possible notifications
+	var notifications = [
+	  "Welcome to Academic department website!",
+	  "Thanks for visiting our site.",
+	  "Mary christmass.",
+	  "Check out our latest blog post.",
+	  "Follow us on social media for updates.",
+	];
+  
+	// Generate a random number between 0 and the length of the array
+	var randomIndex = Math.floor(Math.random() * notifications.length);
+  
+	// Use the random number to select a notification from the array
+	var notification = notifications[randomIndex];
+  
+	// Display the notification using a "toast" style notification
+	var notificationElement = document.createElement("div");
+	notificationElement.classList.add("toast");
+	notificationElement.innerText = notification;
+	document.body.appendChild(notificationElement);
+  
+	// Use a CSS transition to fade the notification in and out
+	notificationElement.classList.add("toast-enter");
+	setTimeout(function() {
+	  notificationElement.classList.remove("toast-enter");
+	  notificationElement.classList.add("toast-exit");
+	  setTimeout(function() {
+		notificationElement.remove();
+	  }, 2000);
+	}, 5000);
+  }
+  
+  // Generate a random notification every 5 seconds
+  setInterval(generateRandomNotification, 13000);
+  
